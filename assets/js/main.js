f104feaa0359d3fe2380d0891cb870847490a1f0
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-03 13:12:50 +0100

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,25 @@
 console.log("test");
 
+/* 
+
+Frage
+
+Array von Bildern
+    image_1.jpg     ...  image_100.jpg
+
+    let retArray =[]
+
+    mit for-Schleife
+        und .push()
+*/
+
+let retArray =[];
+
+let runden = 0;
+
+for (runden = 1; runden < 101 ; runden = runden + 1) {
+    retArray.push(`image_${runden}.jpg`);
+    console.log(retArray);
+    console.table(retArray);
+}
+
